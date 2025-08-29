d4a89a562bdfa00c1c273d21379a7bd6326cf0d9
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2025-08-28 21:14:58 -0300

    Add files via upload

---
+++ SistemaDeEscola/Person.java
@@ -0,0 +1,21 @@
+package SistemaDeEscola;
+
+public abstract class Person {
+    protected String name;
+    protected int age;
+    protected String email;
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+    public abstract String getName();
+    public abstract Person setName(String name);
+
+    public abstract int getAge();
+    public abstract Person setAge(int age);
+    public abstract String getEmail();
+    public abstract Person setEmail(String email);
+    public abstract String getRoleDescription();
+    public abstract String getUniqueIdentifier();
+}

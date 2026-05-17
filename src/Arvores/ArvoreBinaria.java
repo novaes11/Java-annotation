0d81678e96c76a585343777ab6d3a812053aa5f1
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-05-17 16:01:59 -0300

    Feat: Criado o metodo de remoção de nós utilizando recursividade

--- src/Arvores/ArvoreBinaria.java
+++ src/Arvores/ArvoreBinaria.java
@@ -94,6 +94,89 @@ public class ArvoreBinaria {
         }
     }
 
+    public void removerNo(Integer conteudo){
+        No aux = this.raiz;
+        // Antes de remover, verifica se a arvore esta vazia
+        if (estaVazia()){
+            System.out.println("Arvore esta vazia!");
+            return;
+        }
+
+        System.out.println("Removendo o nó " + conteudo + " ...");
+        removerNoRecursivamente(this.raiz, conteudo);
+    }
+
+    private void removerNoRecursivamente(No no, Integer busca) {
+        // Condicao de parada
+        if (no == null) {
+            return;
+        }
+
+        // Busca recursiva -- Vai descer a arvore até encontrar o no com o conteudo de busca
+        if (busca < no.getConteudo()) {
+            removerNoRecursivamente(no.getEsquerda(), busca);
+            return;
+        } else if (busca > no.getConteudo()) {
+            removerNoRecursivamente(no.getDireita(), busca);
+            return;
+        }
+
+
+        // Remoção de nó com 2 filhos
+        if (no.getEsquerda() != null && no.getDireita() != null) {
+            // Encontra o sucessor (menor dos maiores)
+            No sucessor = no.getDireita();
+            while (sucessor.getEsquerda() != null) {
+                sucessor = sucessor.getEsquerda();
+            }
+
+            // Apos encontrar o Nó sucessor, sera substituido o no que sera removido pelo no sucessor
+            no.setConteudo(sucessor.getConteudo());
+
+            // Agora sera removido o no sucessor para que não tenha duplicidade/copia dentro da arvore
+            removerNoRecursivamente(no.getDireita(), sucessor.getConteudo());
+            return;
+        }
+
+        // Remoção de no folha ou de um no com apenas um filho.
+        // OBS: essa parte sera util para a remoção do no sucessor caso o no removido possua 2 filhos
+        No substituto = null;
+        if (no.getEsquerda() != null) {
+            substituto = no.getEsquerda();
+        } else if (no.getDireita() != null) {
+            substituto = no.getDireita();
+        }
+
+        /* Caso o substituto seja != null, o no removido havia 1 filho. Aqui sera determinado que o pai do filho do nó removido, sera o pai do nó removido.
+         *              ANTES:                      DEPOIS:
+         *              PAI                         PAI
+         *  NO REMOVIDO                     FILHO
+         *      |
+         *    FILHO
+         */
+        if (substituto != null) {
+            substituto.setPai(no.getPai());
+        }
+
+        // Caso o pai seja nulo, a unica ocasião é de ser a raiz, pois raiz não possui pai.
+        if (no.getPai() == null) {
+            this.raiz = substituto;
+        }
+        // Caso o nó atual era o filho da ESQUERDA, set o substituto na esquerda.
+        else if (no == no.getPai().getEsquerda()) {
+            no.getPai().setEsquerda(substituto);
+        }
+        // Caso o nó atual era o filho da DIREITA, determinara o substituto na direita
+        else {
+            no.getPai().setDireita(substituto);
+        }
+
+        // Limpar as referências do nó removido para evitar erros na recursão
+        no.setEsquerda(null);
+        no.setDireita(null);
+        no.setPai(null);
+    }
+
     /**
      * Verifica se a árvore está vazia.
      *
